c8f5a7808c966fbdcaf675a9737c5b24debd07a3
Henrik <hesa@sandklef.com>
2020-05-03 23:49:38 +0200

    separating parser and options from main

--- com/sandklef/compliance/cli/LicenseChecker.java
+++ com/sandklef/compliance/cli/LicenseChecker.java
@@ -19,6 +19,8 @@ import org.apache.commons.cli.*;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LicenseChecker {
 
@@ -35,9 +37,7 @@ public class LicenseChecker {
         formatter.printHelp("license-checker.sh", options );
     }
 
-
-    public static void main(String[] args) throws IOException {
-
+    private static Options setupOptions() {
         final Options options = new Options();
         options.addOption(new Option("d", "debug", false, "Turn on debug."));
         options.addOption(new Option("dc", "debug-class", true, "Turn on debug for class only."));
@@ -48,16 +48,10 @@ public class LicenseChecker {
         options.addOption(new Option("c", "component", true, "Component file to check"));
         options.addOption(new Option("h", "help", false, "Print help text"));
         options.addOption(new Option("j", "json", false, "Output result in JSON format"));
+        return options;
+    }
 
-
-        execMode mode = execMode.PRINT_LICENSES;
-
-        String componentFile = null;
-        String licenseDir = "licenses/json";
-        String policyFile = null;
-        LicensePolicy policy = null;
-        ReportExporterFactory.OutputFormat format = ReportExporterFactory.OutputFormat.TEXT;
-
+    private static CommandLineParser parseArguments(String[] args, Options options, Map<String, Object> values) {
         CommandLineParser parser = new DefaultParser();
 
         try {
@@ -75,30 +69,30 @@ public class LicenseChecker {
             }
             if( line.hasOption( "violation" ) ) {
                 Log.d(LOG_TAG, " Checking violations");
-                mode = execMode.CHECK_VIOLATION;
+                values.put("mode",execMode.CHECK_VIOLATION);
             }
             if( line.hasOption( "component" ) ) {
-                componentFile = line.getOptionValue("component");
-                Log.d(LOG_TAG, " Component file: " + componentFile);
+                values.put("componentFile", line.getOptionValue("component"));
+                Log.d(LOG_TAG, " Component file: " + values.get("componentFile"));
             }
             if( line.hasOption( "licenses" ) ) {
-                mode = execMode.PRINT_LICENSES;
+                values.put("mode", execMode.PRINT_LICENSES);
                 Log.d(LOG_TAG, " licenses mode choosend");
             }
             if( line.hasOption( "license-dir" ) ) {
-                licenseDir = line.getOptionValue("license-dir");
-                Log.d(LOG_TAG, " License dir: " + licenseDir);
+                values.put("licenseDir", line.getOptionValue("license-dir"));
+                Log.d(LOG_TAG, " License dir: " + values.get("licenseDir"));
             }
             if( line.hasOption( "policy-file" ) ) {
-                policyFile = line.getOptionValue("policy-file");
-                Log.d(LOG_TAG, " Policy file: " + policyFile);
+                values.put("policyFile", line.getOptionValue("policy-file"));
+                Log.d(LOG_TAG, " Policy file: " + values.get("policyFile"));
             }
             if( line.hasOption( "help" ) ) {
                 help(options);
                 System.exit(0);
             }
             if( line.hasOption( "json" ) ) {
-                format = ReportExporterFactory.OutputFormat.JSON;
+                values.put("format", ReportExporterFactory.OutputFormat.JSON);
             }
 
         }
@@ -107,44 +101,59 @@ public class LicenseChecker {
             System.exit(1);
         }
 
+        return parser;
+    }
 
+    public static void main(String[] args) throws IOException {
+
+        //
+        Options options = setupOptions();
+
+        // Prepare map wth default values
+        Map<String, Object> values = new HashMap<>();
+        values.put("componentFile", null);
+        values.put("licenseDir", "licenses/json");
+        values.put("policyFile", null);
+        values.put("policy", null);
+        values.put("mode", execMode.PRINT_LICENSES);
+
+        // Parse arguments
+        parseArguments(args, options, values);
 
         // Read all licenses
-        Log.d(LOG_TAG, "license dir: " + licenseDir);
-        LicenseStore.getInstance().addLicenses(new JsonLicenseParser().readLicenseDir(licenseDir));
+        Log.d(LOG_TAG, "license dir: " + values.get("licenseDir"));
+        LicenseStore.getInstance().addLicenses(new JsonLicenseParser().readLicenseDir((String) values.get("licenseDir")));
 
-        if (policyFile!=null) {
+        if (values.get("policyFile")!=null) {
             JsonPolicyParser jp = new JsonPolicyParser();
-            policy = jp.readLicensePolicy(policyFile);
-    //        System.out.println("policy: " + policy );
-
-           // System.exit(0);
+            values.put("policy", jp.readLicensePolicy((String) values.get("policyFile")));
         }
-
 
         Log.d(LOG_TAG, "licenses read: " + LicenseStore.getInstance().licenses().size());
 
 
-        if (execMode.PRINT_LICENSES == mode) {
+        if (execMode.PRINT_LICENSES == values.get("mode")) {
             Log.d(LOG_TAG, "printing licenses...");
             System.out.println(LicenseStore.getInstance().licenseString());
-        } else if (execMode.CHECK_VIOLATION == mode) {
-            if (componentFile==null) {
+        } else if (execMode.CHECK_VIOLATION == values.get("mode")) {
+            if (values.get("componentFile")==null) {
                 System.err.println("\n*** Error: missing component file! ***\n\n");
                 help(options);
                 System.err.println("\n\n.... cowardly bailing out.\n");
                 System.exit(1);
             }
 
+            System.out.println("json: " + values.get("format"));
+
             // Read component
-            Log.d(LOG_TAG, "component file: " + componentFile);
+            Log.d(LOG_TAG, "component file: " + values.get("componentFile"));
             JsonComponentParser jp = new JsonComponentParser();
-            Component c = jp.readComponent(componentFile);
+            Component c = jp.readComponent((String) values.get("componentFile"));
             Log.d(LOG_TAG, "Component read: " + c.name());
             Log.d(LOG_TAG, " * deps: " + c.dependencies().size());
 
-            Report report = LicenseArbiter.report(c, policy);
-            System.out.print(ReportExporterFactory.getInstance().exporter(format).exportReport(report)+"\n");
+            Report report = LicenseArbiter.report(c, (LicensePolicy) values.get("policy"));
+            System.out.print(ReportExporterFactory.getInstance().exporter((ReportExporterFactory.OutputFormat) values.get("format")).exportReport(report)+"\n");
         }
     }
 
