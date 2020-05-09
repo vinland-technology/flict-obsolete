b413c11f4cc36910415ea0767749ed1429713022
Henrik <hesa@sandklef.com>
2020-05-09 10:43:33 +0200

    using const string, adapt to domain objects restructure

--- com/sandklef/compliance/exporter/ReportExporter.java
+++ com/sandklef/compliance/exporter/ReportExporter.java
@@ -4,14 +4,15 @@
 
 package com.sandklef.compliance.exporter;
 
-import com.sandklef.compliance.domain.Concern;
-import com.sandklef.compliance.domain.Conclusion;
-import com.sandklef.compliance.domain.LicenseViolation;
-import com.sandklef.compliance.domain.Report;
+import com.sandklef.compliance.domain.*;
+
+import java.util.List;
 
 public interface ReportExporter {
     public String exportReport(Report report);
-    public String exportLicenseViolation(LicenseViolation violation);
-    public String exportConclusion(Conclusion conclusion);
-    public String exportConcern(Concern concern);
+    public String exportLicenseViolations(List<LicenseObligationViolation> violations);
+    public String exportConclusions(List<LicenseConclusion> conclusions);
+    public String exportConcerns(List<PolicyConcern> concerns);
+    public String exportPolicyViolations(List<PolicyViolation> violations);
+    public String exportComponent(Component c);
 }
