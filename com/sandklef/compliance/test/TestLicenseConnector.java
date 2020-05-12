461203dd0115934b6bed6b2855a053e6de80fe35
Henrik <hesa@sandklef.com>
2020-05-12 09:47:36 +0200

    more tests of connector

--- com/sandklef/compliance/test/TestLicenseConnector.java
+++ com/sandklef/compliance/test/TestLicenseConnector.java
@@ -2,6 +2,7 @@ package com.sandklef.compliance.test;
 
 
 import com.sandklef.compliance.domain.*;
+import com.sandklef.compliance.utils.LicenseUtils;
 
 import static com.sandklef.compliance.test.Utils.*;
 
@@ -9,11 +10,44 @@ public class TestLicenseConnector {
 
 
     private static boolean aCanUseB(LicenseConnector a, LicenseConnector b) {
-/*        System.out.println(" a : " + a.license().spdxTag() + " " + a.canBeUsedBy() + " " + a.canUse());
-        System.out.println(" b : " + b.license().spdxTag() + " " + b.canBeUsedBy() + " " + b.canUse());
-        System.out.println(" a : " + a.license().spdxTag() + " " + a.canUse() + " contains " + b.license());
+/*        System.out.println(" a0 : " + a.license() );
+        System.out.println(" a1 : " + a.license().spdxTag() );
+        System.out.println(" a2 : " + a.license().spdxTag() + " " + a.canBeUsedBy() );
   */
-        return a.canUse().contains(b.license());
+        System.out.println("--> aCanUseB : " + a.license().spdxTag() + " " + b.license().spdxTag());
+
+        System.out.println(" a : " + a.license().spdxTag() + " " + a.canBeUsedBy() + " " + a.canUse());
+        System.out.println(" b : " + b.license().spdxTag() + " " + b.canBeUsedBy() + " " + b.canUse());
+
+        System.out.println(" Try 1 ---> : " + a.license().spdxTag() + " " + a.canUse() + " contains " + b.license());
+        if (a.canUse().contains(b)) {
+            return true;
+        }
+        System.out.println(" Try 1 <--- : " + a.license().spdxTag() + " " + a.canUse() + " contains " + b.license());
+
+        // Loop through all b's canBeUsed licenses
+        for (LicenseConnector l : b.canBeUsedBy()) {
+            System.out.println(" Try 2 ---> : " + a.license().spdxTag() + "   can use: " + l.license().spdxTag());
+            if (l.license().spdxTag().equals(a.license().spdxTag())) {
+                System.out.println("  FOUND: " + a.license().spdxTag() + " can use: " + l.license().spdxTag());
+                System.out.println(" Try 2 <--- : " + a.license().spdxTag() + "   can use: " + l.license().spdxTag());
+                return true;
+            }
+            System.out.println(" Try 2 <--- : " + a.license().spdxTag() + "   can use: " + l.license().spdxTag());
+
+            System.out.println(" Try 3 ---> : " + a.license().spdxTag() + "   CHECK: " + l.license().spdxTag());
+            if (aCanUseB(a, l)) {
+                System.out.println(" Try 3 <--- : " + a.license().spdxTag() + "   CHECK: " + l.license().spdxTag());
+                return true;
+            }
+            System.out.println(" Try 3 <--- : " + a.license().spdxTag() + "   CHECK: " + l.license().spdxTag());
+        }
+
+        // non recursive:
+//        return a.canUse().contains(b) ;
+        //
+        System.out.println("<-- aCanUseB : " + a.license().spdxTag() + " CAN NOT " + b.license().spdxTag());
+        return false;
     }
 
     public static void test() {
@@ -24,6 +58,7 @@ public class TestLicenseConnector {
         LicenseConnector gpl20Conn = new LicenseConnector(gpl20);
         LicenseConnector gpl20_laterConn = new LicenseConnector(gpl20_later);
         LicenseConnector apache20Conn = new LicenseConnector(apache20);
+        LicenseConnector gpl30Conn = new LicenseConnector(gpl30);
 
         //   bsd3 ---> lgpl21
         lgpl21Conn.addCanUse(bsd3Conn);
@@ -32,10 +67,32 @@ public class TestLicenseConnector {
         gpl20Conn.addCanUse(lgpl21Conn);
 
         //   gpl20 ---> gpl20_later
-        gpl20Conn.addCanUse(gpl20_laterConn);
+        gpl20_laterConn.addCanUse(gpl20Conn);
 
+        //   gpl20_later ---> gpl30
+        gpl30Conn.addCanUse(gpl20_laterConn);
+
+        // graphically representation
+        // bsd3 ---> lgpl21 ---> gpl20 --> gpl20_later ---> gpl30
+
+        // bsd3 ---> lgpl21
         assertHelper("lgpl21 can use bsd3", aCanUseB(lgpl21Conn, bsd3Conn));
-        assertHelper("bsd3 can use lgpl21", !aCanUseB(bsd3Conn, lgpl21Conn));
+        assertHelper("bsd3 can NOT use lgpl21", (!aCanUseB(bsd3Conn, lgpl21Conn)));
+
+        // bsd3 ---> lgpl21 ---> gpl20
+        assertHelper("gpl20 can use bsd3", aCanUseB(gpl20Conn, bsd3Conn));
+        assertHelper("bsd3 can NOT use gpl20", !aCanUseB(bsd3Conn, gpl20Conn));
+
+        // bsd3 ---> lgpl21 ---> gpl20 --> gpl20_later
+        assertHelper("gpl20_later can use bsd3", aCanUseB(gpl20_laterConn, bsd3Conn));
+        assertHelper("bsd3 can NOT use gpl20_later", !aCanUseB(bsd3Conn, gpl20_laterConn));
+
+        // bsd3 ---> lgpl21 ---> gpl20 --> gpl20_later ---> gpl30
+        assertHelper("gpl30 can use bsd3", aCanUseB(gpl30Conn, bsd3Conn));
+        assertHelper("bsd3 can NOT use gpl30", !aCanUseB(bsd3Conn, gpl30Conn));
+
+        System.out.println(LicenseUtils.listCanUse(gpl30Conn));
+        System.out.println(LicenseUtils.listCanBeUsedBy(bsd3Conn));
 
     }
 
