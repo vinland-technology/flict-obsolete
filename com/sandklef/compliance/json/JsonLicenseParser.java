fe0bc33d09b71fffc7a89a7a539bfb503cfdc086
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:29:31 +0200

    GSON instead of org.json

--- com/sandklef/compliance/json/JsonLicenseParser.java
+++ com/sandklef/compliance/json/JsonLicenseParser.java
@@ -4,27 +4,19 @@
 
 package com.sandklef.compliance.json;
 
-import com.sandklef.compliance.domain.Component;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.sandklef.compliance.domain.License;
-import com.sandklef.compliance.domain.ObligationState;
-import com.sandklef.compliance.utils.LicenseStore;
 import com.sandklef.compliance.utils.Log;
-import com.sandklef.compliance.utils.ObligationBuilder;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static com.sandklef.compliance.domain.Obligation.*;
 
 public class JsonLicenseParser {
 
@@ -50,12 +42,25 @@ public class JsonLicenseParser {
       }
       Log.d(LOG_TAG,"file: " + f);
       License l = readLicense(f.toString());
-      licenses.put(l.spdxTag(), l);
+      licenses.put(l.spdx(), l);
     }
     return licenses;
   }
-  
+
   public License readLicense(String fileName) throws IOException {
+    return readLicenseString(new String(Files.readAllBytes(Paths.get(fileName))));
+  }
+
+  public License readLicenseString(String json) {
+    JsonObject jo = new JsonParser().parse(json).getAsJsonObject();
+    JsonObject licenseJson = jo.get("license").getAsJsonObject();
+    Gson gson = new Gson();
+    License license = gson.fromJson(licenseJson, License.class);
+    return license;
+  }
+
+/*
+      public License readLicense(String fileName) throws IOException {
     return readLicense(new JSONObject(new String(Files.readAllBytes(Paths.get(fileName)))));
   }
 
@@ -64,47 +69,51 @@ public class JsonLicenseParser {
     String swName = JsonUtils.readJsonString(jo, SW_TAG, "");
     int version = JsonUtils.readJsonInt(jo, SW_VERSION_TAG, 0);
 
-    Log.d(LOG_TAG,"meta: " + swName + "   " + version);
+    Log.d(LOG_TAG, "meta: " + swName + "   " + version);
 
     JSONObject lic = jo.getJSONObject("license");
     String name = lic.getString("name");
     String spdx = lic.getString("spdx");
     JSONObject obl = lic.getJSONObject("obligations");
-    Log.d(LOG_TAG,"name: " + name + "   spdx" + spdx);
+    Log.d(LOG_TAG, "name: " + name + "   spdx" + spdx);
 
-    //    System.out.println("  jsonP: " + spdx + "  " + INCLUDE_INSTALL_INSTRUCTIONS + "  " + toObligationState(obl, INCLUDE_INSTALL_INSTRUCTIONS_TAG));
-
-    return new License(spdx,
-                       new ObligationBuilder().
-                       add(LINKING_COPYLEFTED, toObligationState(obl,LINKING_COPYLEFTED_TAG )).
-                       add(MODIFICATION_ALLOWED, toObligationState(obl, MODIFICATION_ALLOWED_TAG)).
-                       add(MODIFICATION_COPYLEFTED, toObligationState(obl, MODIFICATION_COPYLEFTED_TAG)).
-                       add(SUBLICENSING_ALLOWED, toObligationState(obl, SUBLICENSING_ALLOWED_TAG)).
-                       add(DISTRIBUTION_ALLOWED, toObligationState(obl, DISTRIBUTION_ALLOWED_TAG)).
-                       add(DISTRIBUTION_COPYLEFTED, toObligationState(obl, DISTRIBUTION_COPYLEFTED_TAG)).
-                       add(DISCLOSE_SOURCE, toObligationState(obl, DISCLOSE_SOURCE_TAG)).
-                       add(STATE_CHANGES, toObligationState(obl, STATE_CHANGES_TAG)).
-                       add(INCLUDE_COPYRIGHT, toObligationState(obl, INCLUDE_COPYRIGHT_TAG)).
-                       add(INCLUDE_LICENSE, toObligationState(obl, INCLUDE_LICENSE_TAG)).
-                       add(INCLUDE_INSTALL_INSTRUCTIONS, toObligationState(obl, INCLUDE_INSTALL_INSTRUCTIONS_TAG)).
-                       add(INCLUDE_NOTICE_FILE, toObligationState(obl, INCLUDE_NOTICE_FILE_TAG)).
-                       add(INCLUDE_NOTICE_ABOUT_LICENSE, toObligationState(obl, INCLUDE_NOTICE_ABOUT_LICENSE_TAG)).
-                       build());
+    return new License(name, spdx);
   }
+*/
+//    System.out.println("  jsonP: " + spdx + "  " + INCLUDE_INSTALL_INSTRUCTIONS + "  " + toObligationState(obl, INCLUDE_INSTALL_INSTRUCTIONS_TAG));
 
-  private ObligationState toObligationState(JSONObject jo, String key) {
-    Log.d(LOG_TAG,"reading value for key: " + key + " from: " + jo);
-    String value = jo.getString(key);
-    if (value==null) {
-      return ObligationState.UNDEFINED;
-    }
-    if (value.equals("true")) {
-      return ObligationState.TRUE;
-    } else if (value.equals("false")) {
-      return ObligationState.FALSE;
-    }
-    return ObligationState.UNDEFINED;
-  }
+//
+//    return new License(spdx,
+//                       new ObligationBuilder().
+//                       add(LINKING_COPYLEFTED, toObligationState(obl,LINKING_COPYLEFTED_TAG )).
+//                       add(MODIFICATION_ALLOWED, toObligationState(obl, MODIFICATION_ALLOWED_TAG)).
+//                       add(MODIFICATION_COPYLEFTED, toObligationState(obl, MODIFICATION_COPYLEFTED_TAG)).
+//                       add(SUBLICENSING_ALLOWED, toObligationState(obl, SUBLICENSING_ALLOWED_TAG)).
+//                       add(DISTRIBUTION_ALLOWED, toObligationState(obl, DISTRIBUTION_ALLOWED_TAG)).
+//                       add(DISTRIBUTION_COPYLEFTED, toObligationState(obl, DISTRIBUTION_COPYLEFTED_TAG)).
+//                       add(DISCLOSE_SOURCE, toObligationState(obl, DISCLOSE_SOURCE_TAG)).
+//                       add(STATE_CHANGES, toObligationState(obl, STATE_CHANGES_TAG)).
+//                       add(INCLUDE_COPYRIGHT, toObligationState(obl, INCLUDE_COPYRIGHT_TAG)).
+//                       add(INCLUDE_LICENSE, toObligationState(obl, INCLUDE_LICENSE_TAG)).
+//                       add(INCLUDE_INSTALL_INSTRUCTIONS, toObligationState(obl, INCLUDE_INSTALL_INSTRUCTIONS_TAG)).
+//                       add(INCLUDE_NOTICE_FILE, toObligationState(obl, INCLUDE_NOTICE_FILE_TAG)).
+//                       add(INCLUDE_NOTICE_ABOUT_LICENSE, toObligationState(obl, INCLUDE_NOTICE_ABOUT_LICENSE_TAG)).
+//                       build());
+//  }
+//
+//  private ObligationState toObligationState(JSONObject jo, String key) {
+//    Log.d(LOG_TAG,"reading value for key: " + key + " from: " + jo);
+//    String value = jo.getString(key);
+//    if (value==null) {
+//      return ObligationState.UNDEFINED;
+//    }
+//    if (value.equals("true")) {
+//      return ObligationState.TRUE;
+//    } else if (value.equals("false")) {
+//      return ObligationState.FALSE;
+//    }
+//    return ObligationState.UNDEFINED;
+//  }
 
 
 
