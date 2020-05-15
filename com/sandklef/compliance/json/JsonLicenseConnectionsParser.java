dbb10080534613621c7f55f5da81104ee5cabdc2
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:29:19 +0200

    GSON instead of org.json

--- com/sandklef/compliance/json/JsonLicenseConnectionsParser.java
+++ com/sandklef/compliance/json/JsonLicenseConnectionsParser.java
@@ -1,16 +1,23 @@
 package com.sandklef.compliance.json;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import com.sandklef.compliance.domain.License;
 import com.sandklef.compliance.domain.LicenseConnector;
+import com.sandklef.compliance.domain.LicensePolicy;
 import com.sandklef.compliance.utils.LicenseStore;
 import com.sandklef.compliance.utils.Log;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsonLicenseConnectionsParser {
@@ -22,57 +29,56 @@ public class JsonLicenseConnectionsParser {
 
     public static Map<String, LicenseConnector>  readLicenseConnection(String fileName) throws IOException {
         //Log.level(Log.DEBUG);
-        return readLicenseConnection(new JSONObject(new String(Files.readAllBytes(Paths.get(fileName)))));
+        return readLicenseConnectionString(new String(Files.readAllBytes(Paths.get(fileName))));
     }
 
 
-    public static Map<String, LicenseConnector> readLicenseConnection(JSONObject jo) {
+    public static Map<String, LicenseConnector> readLicenseConnectionString(String json) {
         Map<String, LicenseConnector> connectorMap = new HashMap<>();
 
-        // Meta information
-        String swName = JsonUtils.readJsonString(jo, SW_TAG, "");
-        int version = JsonUtils.readJsonInt(jo, SW_VERSION_TAG, 0);
+        JsonObject jo = new JsonParser().parse(json).getAsJsonObject();
+        //        Log.level(Log.DEBUG);
+        Log.d(LOG_TAG, " \n\n\n *** read jo:   " + jo + " ****\n\n");
+        Log.d(LOG_TAG, " \n\n\n *** read meta: " + jo.get("meta").getAsJsonObject() + " ****\n\n");
+        Log.d(LOG_TAG, " \n\n\n *** read conn: " + jo.get("connections").getAsJsonArray() + " ****\n\n");
+        JsonArray connectorsJson = jo.get("connections").getAsJsonArray();
+        Gson gson = new Gson();
+        List<LicenseConnectorIntermediate> connectors = gson.fromJson(connectorsJson, new TypeToken<List<LicenseConnectorIntermediate>>() {}.getType());
 
-        Log.d(LOG_TAG, swName + " " + version);
-        // connections - collect name and create connections
-        JSONArray connectionArrays = jo.getJSONArray("connections");
-        Log.d(LOG_TAG, "connections: " + connectionArrays);
-        for (int i = 0; i < connectionArrays.length(); i++) {
-            // connection
-            JSONObject connection = connectionArrays.getJSONObject(i);
-            String spdx = connection.getString("spdx");
-            Log.d(LOG_TAG, "connectorMap, adding " + spdx);
+        Log.d(LOG_TAG, " \n\n\n *** CONNECTORS: " + connectors + " ****\n\n");
 
-            LicenseConnector lc = new LicenseConnector(LicenseStore.getInstance().license(spdx));
-            connectorMap.put(spdx, lc);
-            Log.d(LOG_TAG, "connectorMap, adding " + lc.license().spdxTag());
+        // connections - collect spdx and create connectors
+        for (LicenseConnectorIntermediate lci : connectors) {
+            Log.d(LOG_TAG, " lc: " + lci.spdx + " >>>> " +  lci.used_by + " \n");
+            LicenseConnector lc = new LicenseConnector(new License(lci.spdx));
+            connectorMap.put(lci.spdx, lc);
         }
-        Log.d(LOG_TAG, "connectorMap: " + connectorMap);
 
-
-        // connections - collect name and create connections
-        connectionArrays = jo.getJSONArray("connections");
-        for (int i = 0; i < connectionArrays.length(); i++) {
-            // connection
-            JSONObject connection = connectionArrays.getJSONObject(i);
-            String spdx = connection.getString("spdx");
-
-            LicenseConnector lc = connectorMap.get(spdx);
-            Log.d(LOG_TAG, " * connector: " + lc.license().spdxTag());
-            // used_by[]
-            JSONArray usedByArray = connection.getJSONArray("used_by");
-            for (int j = 0; j < usedByArray.length(); j++) {
-                // used_by
-                String license = usedByArray.getString(j);
-                LicenseConnector subLc =  connectorMap.get(license);
-                Log.d(LOG_TAG, "   * connector: " + subLc);
-                lc.canBeUsedBy(subLc);
+        // connections - collect name and add connections
+        Log.d(LOG_TAG, " lcs: " + connectors);
+        for (LicenseConnectorIntermediate lci : connectors) {
+            Log.d(LOG_TAG, " lc: " + lci);
+            for (String used_by : lci.used_by) {
+                LicenseConnector lc = connectorMap.get(lci.spdx);
+                LicenseConnector lcToAdd = connectorMap.get(used_by);
+                Log.d(LOG_TAG, "    add to: " + lc + "  " +  lcToAdd + " \n");
+                lc.canBeUsedBy(lcToAdd);
             }
+
         }
-
-        Log.d(LOG_TAG,"meta: " + swName + "   " + version);
-
+        Log.d(LOG_TAG, " return map: " + connectorMap);
         return connectorMap;
     }
 
+    private static class LicenseConnectorIntermediate {
+        private String spdx;
+        private List<String> used_by;
+
+        @Override
+        public String toString() {
+            return "LicenseConnectorIntermediate{" +
+                    "spdx='" + spdx + '\'' +
+                    '}';
+        }
+    }
 }
