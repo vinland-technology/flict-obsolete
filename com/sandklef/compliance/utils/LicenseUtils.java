94afbc1f4c1863bb58be00f85207a3deca2614c2
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:34:18 +0200

    method to print connectors in dot format

--- com/sandklef/compliance/utils/LicenseUtils.java
+++ com/sandklef/compliance/utils/LicenseUtils.java
@@ -2,6 +2,8 @@ package com.sandklef.compliance.utils;
 
 import com.sandklef.compliance.domain.LicenseConnector;
 
+import java.io.PrintStream;
+
 public class LicenseUtils {
 
 
@@ -13,7 +15,7 @@ public class LicenseUtils {
         for (int i=0; i<level; i++) {
             sb.append("| ");
         }
-        sb.append(connector.license().spdxTag());
+        sb.append(connector.license().spdx());
         sb.append("\n");
         for (LicenseConnector l : connector.canUse()){
             sb.append(listCanUse(l, level+1));
@@ -24,17 +26,29 @@ public class LicenseUtils {
     public static String listCanBeUsedBy(LicenseConnector connector) {
         return listCanBeUsedBy(connector, 0);
     }
+
     private static String listCanBeUsedBy(LicenseConnector connector, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i=0; i<level; i++) {
             sb.append("| ");
         }
-        sb.append(connector.license().spdxTag());
+        sb.append(connector.license().spdx());
         sb.append("\n");
         for (LicenseConnector l : connector.canBeUsedBy()){
             sb.append(listCanBeUsedBy(l, level+1));
         }
         return sb.toString();
     }
+
+    public static void connectionsPrintDot(PrintStream writer) {
+        writer.println("digraph depends {\nnode [shape=plaintext]");
+        for ( LicenseConnector lc : LicenseStore.getInstance().connectors().values()) {
+            for (LicenseConnector l : lc.canBeUsedBy()) {
+                writer.println("\"" + lc.license().spdx() +  "\" ->  \"" + l.license().spdx() + "\"");
+            }
+        }
+        writer.println("}");
+    }
+
 
 }
