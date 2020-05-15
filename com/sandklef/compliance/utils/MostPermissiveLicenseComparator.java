d3ebf52a8ffec075a7a358769a61724ea223db99
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:26:18 +0200

    new method to add dep (for GSON compat)

--- com/sandklef/compliance/utils/MostPermissiveLicenseComparator.java
+++ com/sandklef/compliance/utils/MostPermissiveLicenseComparator.java
@@ -10,6 +10,8 @@ import com.sandklef.compliance.domain.*;
 
 public class MostPermissiveLicenseComparator implements Comparator<License> {
 
+  private static final String LOG_TAG = MostPermissiveLicenseComparator.class.getSimpleName();
+
   public int compare(License first, License second) {
 //    System.err.println("WARNING: Dummy implementation of most permissive compare(" +
   //          first + ", " + second);
@@ -18,8 +20,8 @@ public class MostPermissiveLicenseComparator implements Comparator<License> {
             first.isCopyleft() +", " + second.isCopyleft() +"] [" +
             first.spdxTag().compareTo(second.spdxTag()) + " == 1]");
 */
-    // TODO: constantly improve
 
+/*
     if (first.isCopyleft() && !second.isCopyleft()) {
    //   System.err.println("WARNING: Dummy implementation cl vs permissive");
       return 1;
@@ -27,16 +29,25 @@ public class MostPermissiveLicenseComparator implements Comparator<License> {
     if (!first.isCopyleft() && second.isCopyleft()) {
      // System.err.println("WARNING: Dummy implementation permissive vs cl");
       return -1;
+
     }
     if ( first.isCopyleft() == second.isCopyleft()) {
       //System.err.println("WARNING: Dummy implementation type vs type");
       return first.spdxTag().compareTo(second.spdxTag());
     }
-
+*/
    //
     // System.err.println("WARNING: Dummy implementation permissive vs permissive  DEFAULT");
 
-    return first.spdxTag().compareTo(second.spdxTag());
+//    Log.level(Log.DEBUG);
+    Log.d(LOG_TAG, "    compare licenses: " + first + "  " + second + " ===> " + first.spdx().compareTo(second.spdx()));
+    if (first.spdx().contains("GPL")) {
+      Log.d(LOG_TAG, "    compare licenses: " + first + "  " + second + " ===> fake 5");
+      return 5;
+    }
+
+    //TODO: replace this rather sketchy solution
+    return first.spdx().compareTo(second.spdx());
   }
 
   public static final MostPermissiveLicenseComparator comparator =
