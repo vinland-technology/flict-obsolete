bc9cb26ff5ceeb8efd70755a7af03a6c538b0669
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:29:42 +0200

    GSON instead of org.json

--- com/sandklef/compliance/json/JsonPolicyParser.java
+++ com/sandklef/compliance/json/JsonPolicyParser.java
@@ -4,12 +4,13 @@
 
 package com.sandklef.compliance.json;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.sandklef.compliance.domain.LicensePolicy;
 import com.sandklef.compliance.domain.License;
 import com.sandklef.compliance.utils.LicenseStore;
 import com.sandklef.compliance.utils.Log;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -24,10 +25,18 @@ public class JsonPolicyParser {
   public static final String LOG_TAG = JsonPolicyParser.class.getSimpleName();
 
   public LicensePolicy readLicensePolicy(String fileName) throws IOException{
-    return readLicensePolicy(new JSONObject(new String(Files.readAllBytes(Paths.get(fileName)))));
+    Log.d(LOG_TAG, " reading from file: " + fileName);
+    Log.d(LOG_TAG, "content: " + new String(Files.readAllBytes(Paths.get(fileName))));
+    return readLicensePolicyString(new String(Files.readAllBytes(Paths.get(fileName))));
   }
-
-  private LicensePolicy readLicensePolicy(JSONObject jo) {
+  private LicensePolicy readLicensePolicyString(String str) {
+    JsonObject jo = new JsonParser().parse(str).getAsJsonObject();
+    JsonObject polocyJson = jo.get("policy").getAsJsonObject();
+    Gson gson = new Gson();
+    LicensePolicyIntermediate policyGson = gson.fromJson(polocyJson, LicensePolicyIntermediate.class);
+    return policyGson.export();
+  }
+/*  private LicensePolicy readLicensePolicy(JSONObject jo) {
     Log.d(LOG_TAG,"readPolicy");
     List<License> whiteList = readLicenses(jo.getJSONArray("whitelist"));
     List<License> grayList = readLicenses(jo.getJSONArray("graylist"));
@@ -50,8 +59,28 @@ public class JsonPolicyParser {
     }
     return licenses;
   }
+*/
+  private static class LicensePolicyIntermediate {
+    private String name;
+    private List<String> whitelist;
+    private List<String> graylist;
+    private List<String> blacklist;
 
+    private List<License> convert(List<String> stringLicenses) {
+      List<License> licenses = new ArrayList<>();
+      for (String s : stringLicenses) {
+        licenses.add(LicenseStore.getInstance().license(s));
+      }
+      return licenses;
+    }
 
+    public LicensePolicy export() {
+      return new LicensePolicy(name,
+              convert(whitelist),
+              convert(graylist),
+              convert(blacklist));
+    }
+  }
 
 }
 
