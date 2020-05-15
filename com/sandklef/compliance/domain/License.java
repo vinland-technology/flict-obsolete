28e9cfc0bc8aabc64c807d93e6feb7f3a1095091
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-15 17:30:46 +0200

    removing obligations idea spdx instead of spdxTag

--- com/sandklef/compliance/domain/License.java
+++ com/sandklef/compliance/domain/License.java
@@ -14,24 +14,47 @@ public class License {
 
   public static String LOG_TAG = License.class.getSimpleName();
 
-  private String spdxTag;
-  private Map<String, LicenseObligation> obligations;
-  private LicenseType type;
+  private String name;
+  private String spdx;
 
-  /*  public License (String spdxTag, LicenseType type) {
-    this.spdxTag = spdxTag;
+  public String name() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return spdx;
+  }
+
+  public License(String name, String spdx) {
+    this.name = name;
+    this.spdx = spdx;
+  }
+
+  public License(String spdx) {
+    this.spdx = spdx;
+  }
+
+
+  //  private Map<String, LicenseObligation> obligations;
+//  private LicenseType type;
+
+  /*  public License (String spdx, LicenseType type) {
+    this.spdx = spdx;
     this.type = type;
     }*/
-
-  public License (String spdxTag, Map<String, LicenseObligation> obligations) {
-    this.spdxTag = spdxTag;
+/*
+  public License (String spdx, Map<String, LicenseObligation> obligations) {
+    this.spdx = spdx;
     this.obligations = obligations;
   }
+*/
 
   /*  public int compareTo(LicenseType type) {
     return this.type.value() - type.value();
     }*/
 
+  /*
   public static LicenseType concludeType(LicenseType type1, LicenseType type2) {
     Log.dn(LOG_TAG," concluding -- : " + type1.name() + " oder " + type2.toString() + " ==> ");
     if (type1.value() < type2.value() ) {
@@ -41,18 +64,18 @@ public class License {
     Log.d(LOG_TAG, type2.toString());
     return type2;
   }
-  
-  public String spdxTag() {
-    return spdxTag;
+  */
+  public String spdx() {
+    return spdx;
   }
   
-  public LicenseType type() {
-    return type;
-  }
-
-  public Map<String, LicenseObligation> obligations() {
-    return obligations;
-  }
+//  public LicenseType type() {
+//    return type;
+//  }
+//
+//  public Map<String, LicenseObligation> obligations() {
+//    return obligations;
+//  }
 
   /*  public boolean canUse(License l) {
     return LicenseArbiter.canAUseB(this, l);
@@ -61,41 +84,36 @@ public class License {
   public boolean uses(License l) throws LicenseViolationException {
     return LicenseArbiter.aUsesB(this, l);
     }*/
-  
-  @Override
-  public String toString() {
-    return spdxTag;
-  }
 
-  public String toStringLong() {
-    StringBuilder sb = new StringBuilder();
-    sb.append(spdxTag);
-    sb.append("\nObligations:\n");
+  //  public String toStringLong() {
+//    StringBuilder sb = new StringBuilder();
+//    sb.append(spdx);
+//    sb.append("\nObligations:\n");
+//
+//    Log.d(LOG_TAG, "spdx: " + spdx);
+//
+//    for (LicenseObligation obligation : obligations.values()) {
+//      sb.append(" * ");
+//      sb.append(obligation.name());
+//      sb.append(": ");
+//      sb.append(obligation.state());
+//      sb.append("\n");
+//    }
+//    return sb.toString();
+//  }
+//
+//  public LicenseObligation obligation(String obligationName) {
+//    return obligations.get(obligationName);
+//  }
+//
+//  public ObligationState obligationState(String obligationName) {
+//    return obligations.get(obligationName).state();
+//  }
 
-    Log.d(LOG_TAG, "spdxTag: " + spdxTag);
-
-    for (LicenseObligation obligation : obligations.values()) {
-      sb.append(" * ");
-      sb.append(obligation.name());
-      sb.append(": ");
-      sb.append(obligation.state());
-      sb.append("\n");
-    }
-    return sb.toString();
-  }
-
-  public LicenseObligation obligation(String obligationName) {
-    return obligations.get(obligationName);
-  }
-
-  public ObligationState obligationState(String obligationName) {
-    return obligations.get(obligationName).state();
-  }
-
-  public boolean isCopyleft() {
-    return obligation(Obligation.LINKING_COPYLEFTED_NAME).state() ==
-            ObligationState.TRUE;
-  }
+//  public boolean isCopyleft() {
+//    return obligation(Obligation.LINKING_COPYLEFTED_NAME).state() ==
+//            ObligationState.TRUE;
+//  }
 
   public final static String GPL_2_0_SPDX = "GPL-2.0-only";
   public final static String GPL_3_0_SPDX = "GPL-3.0-only";  
