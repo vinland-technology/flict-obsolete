3b22f29add763507f5498f32e2cde39cd82f037c
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-05-25 17:12:12 +0200

    no more org.json

--- bin/license-checker.sh
+++ bin/license-checker.sh
@@ -6,7 +6,7 @@
 
 INSTALL_DIR=$(dirname $(realpath $(which $0)) | sed 's,\/bin,,g')
 
-CLASSPATH="$INSTALL_DIR:$INSTALL_DIR/lib/org.json.jar:$INSTALL_DIR/lib/commons-cli-1.4.jar:lib/gson-2.2.2.jar"
+CLASSPATH="$INSTALL_DIR:$INSTALL_DIR/lib/gson-2.2.2.jar:$INSTALL_DIR/lib/commons-cli-1.4.jar"
 DEFAULT_ARGS=" --license-dir licenses/json "
 CLASS="com.sandklef.compliance.cli.LicenseChecker"
 
