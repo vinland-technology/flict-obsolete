60ec47537f805d3c9057d8b82b67f83c7fd42020
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-07-12 01:37:07 +0200

    Missing MetaData => empty MetaData

--- com/sandklef/compliance/json/JsonMetaInfoParser.java
+++ com/sandklef/compliance/json/JsonMetaInfoParser.java
@@ -1,5 +1,6 @@
 package com.sandklef.compliance.json;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.sandklef.compliance.domain.MetaData;
 
@@ -8,8 +9,19 @@ public class JsonMetaInfoParser {
     public static final String SW_TAG = "software";
     public static final String SW_VERSION_TAG = "version";
 
+    public static String emptyIfNull(JsonElement je) {
+        if (je==null) {
+            return "";
+        }
+        return je.getAsString();
+    }
+
     public static MetaData readMetaData(JsonObject jo) {
-        MetaData md = new MetaData(jo.get(SW_TAG).getAsString(), jo.get(SW_VERSION_TAG).getAsString());
+        if (jo==null) {
+            return new MetaData("", "");
+        }
+        MetaData md = new MetaData(emptyIfNull(jo.get(SW_TAG)),
+                emptyIfNull(jo.get(SW_VERSION_TAG)));
         return md;
     }
 
