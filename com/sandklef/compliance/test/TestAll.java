5229ff340e5b75c7331658e84698f2a4092db575
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-07-12 00:53:44 +0200

    Remove old tests

--- com/sandklef/compliance/test/TestAll.java
+++ com/sandklef/compliance/test/TestAll.java
@@ -14,22 +14,7 @@ import java.util.Map;
 
 public class TestAll {
 
-  private static void testAndPrint(String expr) throws LicenseExpressionException, IllegalLicenseExpression {
-    LicenseExpressionParser lep = new LicenseExpressionParser();
-    LicenseExpression le = lep.parse(expr);
-//    System.out.println("expr: " + expr + "\n  ==>  " + le + " (" + le.paths() + ")\n");
-  }
 
-  
-  private static void testAndPrintFixed(String expr) throws LicenseExpressionException  {
- //   Log.level(Log.DEBUG);
-    expr = expr.replaceAll("\\s", "");
-    LicenseExpressionParser lep = new LicenseExpressionParser();
-    String fixed = lep.fixLicenseExpression(expr);
-//    System.out.println("expr: " + expr + "\n  ==>  " + fixed);
-  }
-
-  
     public static void main(String[] args) throws IOException, LicenseExpressionException, IllegalLicenseExpression, LicenseCompatibility.LicenseConnectorException {
 
       //  System.out.println("\n");
@@ -39,71 +24,6 @@ public class TestAll {
 
       //  System.out.println(" licenses: " + licenses1);
 
-        try {
-
-          LicenseExpressionParser lep = new LicenseExpressionParser();
-          LicenseExpression le;
-
-          
-
-          
-          testAndPrint(" MIT ");
-
-          
-          testAndPrint(" MIT & BSD-3-Clause");
-
-          testAndPrint(" MIT & BSD-3-Clause & Apache-2.0");
-
-          testAndPrint(" ( MIT ) ");
-
-
-          testAndPrint(" ( ( MIT ) ) ");
-
-
-          testAndPrint(" LGPL-2.1-only ");
-
-
-          testAndPrint(" ( LGPL-2.1-only ) ");
-
-
-          testAndPrint(" (( LGPL-2.1-only ) )  ");
-
-
-          testAndPrint(" ( ( ( ( LGPL-2.1-or-later & GPL-2.0-or-later ) ) ) )");
-
-          testAndPrint("  ( ( LGPL-2.1-or-later & GPL-2.0-or-later ) & BSD-3-Clause ) ");
-
-          testAndPrint("  ( ( LGPL-2.1-or-later & GPL-2.0-or-later ) & BSD-3-Clause ) & ( BSD-3-Clause & MIT )");
-
-          testAndPrint("  ( ( LGPL-2.1-or-later | GPL-2.0-or-later ) | BSD-3-Clause )");
-
-          testAndPrint(" LGPL-2.1-or-later | GPL-2.0-or-later & BSD-3-Clause  & MIT  & MIT");
-          testAndPrint(" LGPL-2.1-or-later & GPL-2.0-or-later | BSD-3-Clause  ");
-          testAndPrint(" LGPL-2.1-or-later & GPL-2.0-or-later | LGPL-2.1-only | BSD-3-Clause & MIT   ");
-
-
-          testAndPrint(" LGPL-2.1-or-later | GPL-2.0-or-later & BSD-3-Clause  ");
-
-
-
-          testAndPrint("(  ( ( LGPL-2.1-or-later & GPL-2.0-or-later ) & BSD-3-Clause ) & ( BSD-3-Clause & MIT ) & (GPL-2.0-or-later & BSD-3-Clause) )");
-
-
-            System.out.println("\n");
-            System.out.println("\n");
-            System.out.println("\n");
-            testAndPrint("( GPL-2.0-or-later & MIT )  | ( LGPL-2.1-or-later & BSD-3-Clause)");
-
-            testAndPrint("( GPL-2.0-or-later & MIT )  | ( LGPL-2.1-or-later & BSD-3-Clause) | ( GPL-2.0-or-later)");
-
-            testAndPrint("GPL-2.0-or-later | MIT | LGPL-2.1-or-later | BSD-3-Clause | GPL-2.0-or-later");
-
-
-        } catch (LicenseExpressionException | IllegalLicenseExpression e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("\n");
 
         TestJsonComponentParser.test();
         System.out.println("\n");
